db79839da56b3ef9aedc7d04b5db3d0b6636ac09
shitao <shitao19_88@163.com>
2016-06-08 15:10:49 +0800

    first

--- leaflet-src.js
+++ leaflet-src.js
@@ -3821,6 +3821,7 @@ L.Popup = L.Class.extend({
 		closeButton: true,
 		offset: [0, 7],
 		autoPanPadding: [5, 5],
+		position:'top',
 		// autoPanPaddingTopLeft: null,
 		// autoPanPaddingBottomRight: null,
 		keepInView: false,
@@ -3988,6 +3989,10 @@ L.Popup = L.Class.extend({
 
 		this._tipContainer = L.DomUtil.create('div', prefix + '-tip-container', container);
 		this._tip = L.DomUtil.create('div', prefix + '-tip', this._tipContainer);
+		if (this.options.position) {
+			L.DomUtil.addClass(this._tipContainer,this.options.position );
+			L.DomUtil.addClass(this._tip, this.options.position);
+		}
 	},
 
 	_updateContent: function () {
@@ -4032,6 +4037,7 @@ L.Popup = L.Class.extend({
 		}
 
 		this._containerWidth = this._container.offsetWidth;
+		this._containerHeight= this._container.offsetHeight;
 	},
 
 	_updatePosition: function () {
@@ -4045,12 +4051,24 @@ L.Popup = L.Class.extend({
 			L.DomUtil.setPosition(this._container, pos);
 		}
 
-		this._containerBottom = -offset.y - (animated ? 0 : pos.y);
-		this._containerLeft = -Math.round(this._containerWidth / 2) + offset.x + (animated ? 0 : pos.x);
+
+        if (this.options.position=='right') {
+        	 this._containerBottom = -Math.round(this._containerHeight / 2) -offset.y + (animated ? 0 : pos.y);
+		     this._containerLeft =  offset.x+27 - (animated ? 0 : pos.x);
+		     this._tipContainer.style.bottom=this._containerHeight/2 -10+ 'px';
+	     	this._tipContainer.style.left=-this._containerWidth/2-10+ 'px';
+        }else{
+        	this._containerBottom = -offset.y - (animated ? 0 : pos.y);
+		    this._containerLeft = -Math.round(this._containerWidth / 2) + offset.x + (animated ? 0 : pos.x);
+
+        }
+       
 
 		// bottom position the popup in case the height of the popup changes (images loading etc)
 		this._container.style.bottom = this._containerBottom + 'px';
 		this._container.style.left = this._containerLeft + 'px';
+
+		
 	},
 
 	_zoomAnimation: function (opt) {
